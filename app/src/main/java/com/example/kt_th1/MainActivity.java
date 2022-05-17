44386eae5f9fd2faeb8d918844273d2266b87223
tuongtocdo399 <97137705+tuongtocdo399@users.noreply.github.com>
2022-05-17 09:57:19 +0700

    upload

---
+++ app/src/main/java/com/example/kt_th1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kt_th1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void xulyThoat (View view) {
+        finish();
+    }
+
+}
